8cfcdecc1948f1d27ee1a1a3afcadbf25949efaf
do-oni <dxxneo@gmail.com>
2021-11-28 23:46:18 +0900

    initial commit

---
+++ src/test/java/kr/pe/loginapi/LoginapiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.loginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
